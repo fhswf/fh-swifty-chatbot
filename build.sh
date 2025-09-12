c5e836482094419ffe49adcf9a12216a6b101d38
Donald Kapyouo <dkapyouo@gmail.com>
2025-09-12 13:39:47 +0200

    chore: enhance build script to include Docker login and push commands

--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
 docker build --platform linux/amd64 -t ghcr.io/fhswf/fh-swifty-chatbot:latest .
+echo $GITHUB_TOKEN | docker login ghcr.io -u username --password-stdin
+docker push ghcr.io/fhswf/fh-swifty-chatbot:latest 
